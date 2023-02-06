a6d6367478c377e95ec891f26b3c642b7318de05
SerMeliodas <oliver9574@gmail.com>
2023-02-06 21:06:53 +0100

    revrited lsp server

---
+++ nvim/init.lua
@@ -0,0 +1,12 @@
+--imports
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+require("ser.autocomplete_settings")
+require("ser.lsp_settings")
+require("ser.mason")
+require("ser.lualine_settings")
+require("ser.nvimTree_settings")
+require("ser.base")
+require("ser.plugins")
+require("ser.maps")
+
